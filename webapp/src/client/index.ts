d48350d8246de3997c11765e6a1407c027f3629d
strandgeek <strandgeek@gmail.com>
2022-06-23 02:25:11 -0300

    fix(webapp): use relative api base URL

--- webapp/src/client/index.ts
+++ webapp/src/client/index.ts
@@ -7,7 +7,7 @@ import axios from "axios";
 export const queryClient = new QueryClient()
 
 export const client =  axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "/api",
   headers: {
     "Content-type": "application/json"
   }
